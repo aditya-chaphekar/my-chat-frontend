7d6933967d3c69131a8919bfb8e23bb5d203c85b
Aditya <aditya@hubilo.com>
2022-05-25 14:30:53 +0530

    added sass to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import ReactDOM from 'react-dom/client'
+import './styles/app.sass'
+import App from './pages/App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<App />)
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,3 @@
-import './App.css'
-
 const App = () => <div className='App'>Hello World</div>
 
 export default App
