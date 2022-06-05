a2560f47902404404c573175ab38c8c94ac6ae52
Mohit <mohitmchordiya33@gmail.com>
2022-06-05 18:03:01 +0530

    Login page UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,44 @@
-const Login = () => {
-  console.log('Login')
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Grid, Hidden, InputAdornment, TextField, Typography } from '@mui/material'
+
+const Login = () => (
+  <Grid container justifyContent='center'>
+    <Hidden smDown>
+      <Grid item sm={4} />
+    </Hidden>
+    <Grid
+      item
+      xs={12}
+      md={4}
+      p={{ xs: 3, md: 10 }}
+      textAlign='center'
+      justifyContent='center'
+      direction='column'
+    >
+      <Typography fontStyle='bold' variant='h3' my={{ xs: 5, md: 10 }}>
+        Log in.
+      </Typography>
+      <TextField
+        sx={{ width: '300px', paddingBottom: '50px' }}
+        placeholder='Phone Number'
+        InputProps={{
+          // className: 'mobileNumber',
+          startAdornment: (
+            <InputAdornment position='start'>
+              <FontAwesomeIcon icon={faMobileAlt} />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button variant='contained' sx={{ width: '300px' }}>
+        Send OTP
+      </Button>
+    </Grid>
+    <Hidden mdDown>
+      <Grid item md={4} />
+    </Hidden>
+  </Grid>
+)
 
 export default Login
